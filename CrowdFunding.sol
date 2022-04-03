a9fa96890eba8802cd0d9a4c981cdc7850b1f8b8
Sparsh09 <sparsh1698.cse18@chitkara.edu.in>
2022-04-03 18:00:21 +0530

    added the vote and send functionalities

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -9,13 +9,50 @@ contract CrowdFunding{
     uint public raisedAmount;
     uint public nuOfContributors;
 
+    struct Request{
+        string description;
+        address payable recipent;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address=>bool) voters;
+    }
+    mapping(uint=>Request) public requests;
+    uint public nuOfRequests;
     constructor(uint _target,uint _deadline) public {
         target = _target;
         deadline = block.timestamp + _deadline;
         minContribution = 100 wei;
         manager = msg.sender;
     }
-
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only Manager can call this function ");
+        _;
+    }
+    function createRequest(string memory _description,address payable _recipent,uint _value) public onlyManager {
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.description = _description;
+        newRequest.recipent = _recipent;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;  
+    }
+    function voteRequest(uint _requestNo) public {
+        require(contributors[msg.sender] > 0,"You must be a contributor");
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.voters[msg.sender]==false,"You have already voted for this request");
+        thisRequest.voters[msg.sender] = true;
+        thisRequest.noOfVoters++;
+    }
+    function makePayment(uint _requestNo) public onlyManager{
+        require(raisedAmount>=target);
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.completed == false ,"This request has been completed");
+        require(thisRequest.noOfVoters > nuOfContributors / 2,"Majority does not support the request");
+        thisRequest.recipent.transfer(thisRequest.value);
+        thisRequest.completed = true;
+    }
     function sendEther() public payable{
         require(block.timestamp < deadline,"Deadline has passed");
         require(msg.value >= minContribution,"Minimum contribution is not met");
@@ -28,4 +65,12 @@ contract CrowdFunding{
     function getContractBalance() public view returns(uint) {
         return address(this).balance;
     }
+    function refund() public {
+        require(block.timestamp > deadline && raisedAmount < target,"You are not eligible for the refund"); 
+        require(contributors[msg.sender]>0);
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender] = 0 ;
+    }
+    
 }
